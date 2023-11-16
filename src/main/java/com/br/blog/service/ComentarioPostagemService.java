8ad059aa100420754aac90f482744b77c3da84bf
Sanny Helen Lima <sannysilvalima@gmail.com>
2023-11-16 06:50:09 -0300

    Criação do service para comentários

---
+++ src/main/java/com/br/blog/service/ComentarioPostagemService.java
@@ -0,0 +1,8 @@
+package com.br.blog.service;
+
+import com.br.blog.model.dto.ComentarioPostagemDTO;
+
+public interface ComentarioPostagemService {
+
+    Integer salvar(ComentarioPostagemDTO comentarioPostagemDTO);
+}
