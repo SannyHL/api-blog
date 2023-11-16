f06ac7b6869567a15eb99a6cd9923a89e61c197f
Sanny Helen Lima <sannysilvalima@gmail.com>
2023-11-16 07:29:38 -0300

    Ajustes de atributos.

--- src/main/java/com/br/blog/model/Comentario.java
+++ src/main/java/com/br/blog/model/Comentario.java
@@ -22,16 +22,27 @@ public class ComentarioPostagem {
     private Integer id;
     @Column(name = "TEXTO")
     private String texto;
-    @Column(name = "DATA_CRIACAO")
-    private Date dataCriacao;
     @ManyToOne
     @JoinColumn(name = "POSTAGEM_ID")
     private Postagem postagem;
+    @ManyToOne
+    @JoinColumn(name = "USUARIO_ID")
+    private Usuario usuario;
+    @Column(name = "DATA_CRIACAO")
+    private Date dataCriacao;
+    @Column(name = "DATA_ALTERACAO")
+    private Date dataAlteracao;
+    @Column(name = "ATIVO")
+    private Boolean ativo;
+
 
     public ComentarioPostagem(ComentarioPostagemDTO comentario) {
         this.id = comentario.getId();
         this.texto = comentario.getTexto();
         this.dataCriacao = comentario.getDataCriacao();
+this.dataAlteracao = comentario.getDataAlteracao();
         this.postagem = new Postagem(comentario.getPostagemId());
+ this.usuario = new Usuario(comentario.getUsuarioId());
+ this.ativo = comentario.getAtivo();
     }
 }
