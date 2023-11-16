7b0c88cd6aa42adfc37114dc1d6db842a7211686
Sanny Helen Lima <sannysilvalima@gmail.com>
2023-11-16 12:47:57 -0300

    Ajustes e implementação de novos methodos.

--- src/main/java/com/br/blog/service/serviceImpl/ComentarioPostagemServiceImpl.java
+++ src/main/java/com/br/blog/service/serviceImpl/ComentarioPostagemServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ComentarioPostagemServiceImpl implements ImagensPostagemService {
+public class ComentarioPostagemServiceImpl implements ComentarioPostagemService {
 
     @Autowired
     ComentarioPostagemRepository comentarioPostagemRepository;
@@ -18,5 +18,51 @@ public class ComentarioPostagemServiceImpl implements ImagensPostagemService {
         ComentarioPostagem comentarioPostagem = new ComentarioPostagem(comentarioPostagemDTO);
         comentarioPostagemRepository.saveAndFlush(comentarioPostagem);
         return comentarioPostagem.getId();
+    }
+
+    @Override
+    public Integer atualizar(ComentarioPostagemDTO comentarioPostagemDTO) {
+        try {
+            ComentarioPostagemDTO comentarioPostagemNoBanco = buscarPorId(comentarioPostagemDTO.getId());
+            comentarioPostagemDTO.setAtivo(comentarioPostagemNoBanco.getAtivo());
+            comentarioPostagemDTO.setUsuarioId(comentarioPostagemNoBanco.getUsuarioId());
+            comentarioPostagemDTO.setDataCriacao(comentarioPostagemNoBanco.getDataCriacao());
+            comentarioPostagemDTO.setDataAlteracao(new Date());
+            ComentarioPostagem comentarioPostagem = new ComentarioPostagem(comentarioPostagemDTO);
+            comentarioPostagemRepository.saveAndFlush(comentarioPostagem);
+            return postagem.getId();
+        } catch (ResponseStatusException e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao editar comentario.");
+        }
+    }
+
+    @Override
+    public List<ComentarioPostagemDTO> buscarTodosComentariosAtivos() {
+        try{
+            return comentarioPostagemRepository.buscarTodosComentariosAtivos();
+        } catch (ResponseStatusException e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao buscar comentários.");
+        }
+    }
+
+    @Override
+    public ComentarioPostagemDTO buscarPorId(Integer comentarioId) {
+        try{
+            return comentarioPostagemRepository.buscarPorId(comentarioId);
+        } catch (ResponseStatusException e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao buscar comentário.");
+        }
+    }
+
+    @Override
+    public void deletar(Integer comentarioId) {
+        try{
+            ComentarioPostagemDTO comentarioDTOBanco = buscarPorId(comentarioId);
+            comentarioDTOBanco.setAtivo(false);
+            ComentarioPostagem comentarioPostagem = new Postagem(comentarioDTOBanco);
+            comentarioPostagemRepository.saveAndFlush(comentarioPostagem);
+        } catch (ResponseStatusException e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao deletar comentário.");
+        }
     }
 }
