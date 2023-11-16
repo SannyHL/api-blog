4aea68f6a22b7050691c8d0a408316005ef64eab
Sanny Helen Lima <sannysilvalima@gmail.com>
2023-11-16 07:03:07 -0300

    Criação de controller para o crud.

---
+++ src/main/java/com/br/blog/controller/ComentarioPostagemController.java
@@ -0,0 +1,44 @@
+package com.br.blog.controller;
+
+
+import com.br.blog.model.dto.ComentarioPostagemDTO;
+import com.br.blog.service.ComentarioPostagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/postagens/comentarios")
+public class ComentarioPostagemController {
+
+    @Autowired
+    ComentarioPostagemService comentarioPostagemService;
+
+    @PostMapping
+    public ResponseEntity<Integer> salvar(@RequestBody ComentarioPostagemDTO comentarioPostagemDTO){
+        return ResponseEntity.ok(comentarioPostagemService.salvar(comentarioPostagemDTO));
+    }
+
+    @GetMapping
+    public  ResponseEntity<List<ComentarioPostagemDTO>> buscarTodosAtivos(){
+        return ResponseEntity.ok(comentarioPostagemService.buscarTodosAtivos());
+
+    }
+    @GetMapping("/{id}")
+    public  ResponseEntity<ComentarioPostagemDTO> buscarPorId(@PathVariable("id") Integer id){
+        return ResponseEntity.ok(comentarioPostagemService.buscarPorId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar(@PathVariable("id") Integer id){
+        comentarioPostagemService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public  ResponseEntity<Integer> atualizar(@RequestBody ComentarioPostagemDTO comentarioPostagemDTO){
+        return ResponseEntity.ok(comentarioPostagemService.atualizar(comentarioPostagemDTO));
+    }
+}
