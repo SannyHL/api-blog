8e76c8a5492bbb8cf03d67c2bb33e7ec3e153c55
Sanny Helen Lima <sannysilvalima@gmail.com>
2023-11-16 07:23:11 -0300

    Atualizar o classe.

--- src/main/java/com/br/blog/model/dto/ComentarioPostagemDTO.java
+++ src/main/java/com/br/blog/model/dto/ComentarioPostagemDTO.java
@@ -14,12 +14,18 @@ public class ComentarioPostagemDTO {
     private Integer id;
     private String texto;
     private Date dataCriacao;
+    private Date dataAlteracao;
     private Integer postagemId;
+    private Integer usuarioId;
+    private Boolean ativo;
 
-    public ComentarioPostagemDTO(Integer id, String texto, Date dataCriacao, Integer postagemId) {
+    public ComentarioPostagemDTO(Integer id, String texto,Integer usuarioId,Integer postagemId, Date dataCriacao, Date dataAlteracao, Boolean ativo) {
         this.id = id;
         this.texto = texto;
+        this.usuarioId = usuarioId;
+        this.postagemId = postagemId
         this.dataCriacao = dataCriacao;
-        this.postagemId = postagemId;
+        this.dataAlteracao = dataAlteracao;
+        this.ativo = ativo;
     }
 }
