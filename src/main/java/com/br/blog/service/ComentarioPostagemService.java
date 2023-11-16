1e20e8c7e889a70d6fe59f8b9868747a4ffc6efc
Sanny Helen Lima <sannysilvalima@gmail.com>
2023-11-16 12:50:16 -0300

    Inserção de métodos.

--- src/main/java/com/br/blog/service/ComentarioPostagemService.java
+++ src/main/java/com/br/blog/service/ComentarioPostagemService.java
@@ -5,4 +5,9 @@ import com.br.blog.model.dto.ComentarioPostagemDTO;
 public interface ComentarioPostagemService {
 
     Integer salvar(ComentarioPostagemDTO comentarioPostagemDTO);
+    Integer atualizar(ComentarioPostagemDTO comentarioPostagemDTO);
+    List<ComentarioPostagemDTO> buscarTodosComentariosAtivos();
+    ComentarioPostagemDTO buscarPorId(Integer comentarioId);
+    void deletar(Integer comentarioId);
+    
 }
